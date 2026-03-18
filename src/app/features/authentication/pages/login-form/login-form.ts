586709b37db5f3fe0a0b8323ecc2df7e681b29ac
Pedro Veras <veraspedro1608@gmail.com>
2026-03-18 12:09:22 -0300

    feat: ✨ implementando logica de login com rxjs

--- src/app/features/authentication/pages/login-form/login-form.ts
+++ src/app/features/authentication/pages/login-form/login-form.ts
@@ -1,5 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { email, Field, form, minLength, required } from '@angular/forms/signals';
+import { UserApi } from '../../../../core/services/user-api';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login-form',
@@ -8,6 +12,12 @@ import { email, Field, form, minLength, required } from '@angular/forms/signals'
   styleUrl: './login-form.css',
 })
 export class LoginForm {
+  private readonly _userApi = inject(UserApi);
+  private readonly _router = inject(Router);
+  private readonly _destroyRef = inject(DestroyRef);
+  
+  loginErrorMessage = signal<string>('')
+  
   loginModel = signal({
     email: '',
     password: '',
@@ -20,4 +30,19 @@ export class LoginForm {
     required(fieldPath.password, { message: 'A senha é obrigatória' });
     minLength(fieldPath.password, 8, { message: 'A senha deve ter no mínimo 8 caracteres' });
   })
+
+  login() {
+    const { email, password } = this.loginForm().value();
+
+    this._userApi.login(email, password).pipe(
+      takeUntilDestroyed(this._destroyRef)
+    ).subscribe({
+      next: () => {
+        this._router.navigate(['/explore']);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.loginErrorMessage.set(error.error.message)
+      }
+    });
+  }
 }
